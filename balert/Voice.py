9e3fca988b3c216007c123c59c19c72c8863d820
Tushar Gautam <tushar.rishav@gmail.com>
2015-08-18 19:00:40 +0530

    fetch custom message and other setup from config

--- balert/Voice.py
+++ balert/Voice.py
@@ -1,23 +1,34 @@
+from Config import Config
 import pyttsx
 
 
-class voice:
+class voice(Config):
 	
 	def __init__ (self, _ = 120):
 		self.engine = pyttsx.init()
 		self.rate = _
 		self.engine.setProperty('rate', self.rate)
 		self.msg = ""
+		
 	
-	def set_rate(self,rate):
-		self.engine.setProperty('rate', rate)
+	def set_rate(self):
+		lp = self.load_pickle()
+		self.engine.setProperty('rate',lp["RATE"])
 
-	def set_vol(self, volume):
-		self.engine.setProperty('volume', volume)
+	def set_vol(self):
+		lp = self.load_pickle()
+		self.engine.setProperty('volume', lp["VOL"])
 
-	def set_lang(self, lang):
-		self.engine.setProperty('voice', lang)
+	def set_lang(self):
+		lp = self.load_pickle()
+		self.engine.setProperty('voice', lp["LANG"])
 	
-	def speak(self):
-		self.engine.say(self.msg)
+	def speak(self,msg):
+		lp = self.load_pickle()
+		self.engine.say(lp["MSG"] + msg)
 		self.engine.runAndWait()
+
+	def one_thing(self):
+		self.set_lang()
+		self.set_vol()
+		self.set_rate()
