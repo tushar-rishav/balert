0b17f29b99046fae1300ae41d4a2788c51f9d742
Tushar Gautam <tushar.rishav@gmail.com>
2015-08-20 23:56:42 +0530

    Try-Catch

--- tests/test.py
+++ tests/test.py
@@ -4,11 +4,11 @@ class BalertTestCase(unittest2.TestCase):
     """Tests for `balert/main.py`."""
     
     def run_balert(self):
-    	balert_run = subprocess.Popen("balert", shell=True, stdout=subprocess.PIPE).stdout.read()
-    	if "Ok" in balert_run:
-        	return True
-        else:
-        	return False
+    	try:
+    		balert_run = subprocess.Popen("balert", shell=True, stdout=subprocess.PIPE).stdout.read()
+    		return True
+    	except:
+			return False
 
     def test_balert(self):
 		self.assertTrue(self.run_balert())
