0ba2fe68151da8cdf18ac7d0af01e8d010bc6225
Tushar Gautam <tushar.rishav@gmail.com>
2015-08-21 20:35:10 +0530

    PEP8 conformations

--- balert/main.py
+++ balert/main.py
@@ -1,21 +1,28 @@
 #! /usr/bin/python
-from sys import argv, exit ,version_info
-from Bsettings import bpath,SetLevel
+from sys import argv, exit, version_info
+from Bsettings import bpath, SetLevel
 from Voice import voice
 from BatteryStatus import battery
 from Config import Config
-import argparse,logging,subprocess,os
+import argparse
+import logging
+import subprocess
+import os
 
 if version_info >= (3,):
     import pickle
 else:
-    import cPickle as pickle # efficient version
+    import cPickle as pickle  # efficient version
+
 
 def setupCron():
     try:
-        location_f = subprocess.Popen("whereis balert", shell=True, stdout=subprocess.PIPE).stdout.read().strip().split(':')[1].strip()
-        cmd = subprocess.Popen("crontab -l", shell=True, stdout=subprocess.PIPE).stdout.read()
-        if not ('balert' in cmd): # avoid multiple cronjob creation
+        location_f = subprocess.Popen("whereis \
+        balert", shell=True, stdout=subprocess.PIPE).stdout.read().\
+            strip().split(':')[1].strip()
+        cmd = subprocess.Popen("crontab -l", shell=True,
+                               stdout=subprocess.PIPE).stdout.read()
+        if not ('balert' in cmd):    # avoid multiple cronjob creation
             cmd += "*/10 * * * * " + location_f + "\n"
             tmp = open("/tmp/temp_cron.impossible", 'w')
             tmp.write(cmd)
@@ -23,25 +30,25 @@ def setupCron():
             subprocess.Popen("crontab /tmp/temp_cron.impossible", shell=True)
             logging.info("Successfully set up the cron job.")
             print "Ok"  # Do not remove this line ever!
-                
     except:
         logging.debug("Error writing the cron job.")
 
+
 def main():
-    
     logging.getLogger().setLevel(logging.DEBUG)
     parser = argparse.ArgumentParser(description=" \
-             Listen the voice of your battery whenever she is low!",epilog="Author:\
-             tushar.rishav@gmail.com")
+             Listen the voice of your battery whenever she is low!", epilog="\
+             Author:tushar.rishav@gmail.com")
     parser.add_argument("-r", "--rate", help="Rate of speaking.(100-200)",
-                       type=int)
+                        type=int)
     parser.add_argument("-v", "--vol", help="Volume of speaking.(1.0)",
-                       type=str)
+                        type=str)
     parser.add_argument("-l", "--lang", help="Language speaking.(1.0)",
-                       type=str)
+                        type=str)
     parser.add_argument("-m", "--msg", help="Alert message of your own",
-                       type=str)
-    parser.add_argument("-c", "--charge", help="Decide the critical charge level",type=int)
+                        type=str)
+    parser.add_argument("-c", "--charge", help="Decide the critical charge\
+    level", type=int)
     args = parser.parse_args()
     if len(argv) == 1:
         pass
@@ -59,17 +66,16 @@ def main():
     if args.charge:
         cf_data["CHARGE"] = args.charge
     cf.set_pickle(cf_data)
-    
     # READ BATTERY
-    __ = battery()
-    _ = __.get_low_battery_warning_level()
-    logging.debug( _ )
+    battery_instance = battery()
+    charge_info = battery_instance.get_low_battery_warning_level()
+    logging.debug(charge_info)
     # OUTPUT
 
-    if _[0] == 0 and _[1]:
-        add_msg = " All cool! %d Percent remaining" %_[1]
-    elif _[0] == 1:
-        add_msg = " Low Battery! %d Percent remaining" %_[1]
+    if charge_info[0] == 0 and charge_info[1]:
+        add_msg = " All cool! %d Percent remaining" % charge_info[1]
+    elif charge_info[0] == 1:
+        add_msg = " Low Battery! %d Percent remaining" % charge_info[1]
         logging.info(cf_data["MSG"]+add_msg)
         cf.set_pickle(cf_data)
         al.speak(add_msg)
@@ -77,12 +83,5 @@ def main():
         add_msg = " Battery is Charging!"
     setupCron()
 
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
-    
