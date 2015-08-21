bd561f2e82d7946fb150a44f0f502d29ddc165c2
Tushar Gautam <tushar.rishav@gmail.com>
2015-08-21 20:59:46 +0530

    PEP8 conformations

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,20 @@
-import subprocess,unittest2
+import subprocess
+import unittest2
+
 
 class BalertTestCase(unittest2.TestCase):
     """Tests for `balert/main.py`."""
-    
+
     def run_balert(self):
-    	try:
-    		balert_run = subprocess.Popen("balert", shell=True, stdout=subprocess.PIPE).stdout.read()
-    		return True
-    	except:
-			return False
+        try:
+            balert_run = subprocess.Popen("balert\
+                ", shell=True, stdout=subprocess.PIPE).stdout.read()
+            return True
+        except:
+            return False
 
     def test_balert(self):
-		self.assertTrue(self.run_balert())
+        self.assertTrue(self.run_balert())
 
 if __name__ == "__main__":
     unittest2.main()
